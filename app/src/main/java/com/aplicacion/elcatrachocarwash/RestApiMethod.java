c9cb851ade2199372e9b9150135eda579cbf95a5
M4rv1nMMM <marvinmiguel3@gmail.com>
2021-08-08 15:19:42 -0600

    Eliminacion de Vehiculos desde PerfilVehiculoFragment.java

--- app/src/main/java/com/aplicacion/elcatrachocarwash/RestApiMethod.java
+++ app/src/main/java/com/aplicacion/elcatrachocarwash/RestApiMethod.java
@@ -7,7 +7,7 @@ public class RestApiMethod {
     public static final String ApiGetModels = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/buscar_modelos.php";
     public static final String ApiGetOil = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/buscar_aceite.php";
     public static final String ApiGetServices = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/buscar_servicios.php";
-    public static final String ApiDeleteUrl = "https://stephanyweb.000webhostapp.com/api/eliminar.php";
+    public static final String ApiDeleteVehicleUrl = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/eliminar_vehiculo.php";
     public static final String ApiPutUrlClient = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/modificar_cliente.php";
     public static final String ApiPostCotizacionUrl = "https://dandsol.000webhostapp.com/ElCatrachoCarwash/crear_cotizacion.php";
 
